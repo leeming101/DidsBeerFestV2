b5238e141235a4f8a5a6a2b34feb625b1ce376ea
Paul Leeming <Paul.Leeming@fioffice.net>
2016-10-05 10:22:45 +0100

    refactor sponsor list to prevent console errors & reduce html markup repetition

--- app/scripts/controllers/sponsors.js
+++ app/scripts/controllers/sponsors.js
@@ -11,22 +11,18 @@ angular.module('didsBeerFestV2App')
   .controller('SponsorsCtrl', function ($scope) {   
       
       $scope.sponsorList = [
-        '<a href="http://smcreditunion.co.uk/"><img src="images/sponsors/1.png"></a>',
-        '<a href="http://www.bottonline.co.uk/"><img src="images/sponsors/2.png"></a>',
-        '<a href="http://www.wearecreation.co.uk/"><img src="images/sponsors/3.png"></a>',
-        '<a href="http://www.claytonhulme.co.uk/"><img src="images/sponsors/4.png"></a>',
-        '<a href="https://twitter.com/Wheildons/"><img src="images/sponsors/5.png"></a>',
-        '<img src="images/sponsors/6.png">',
-        '<a href="http://www.barneswalker.co.uk"><img src="images/sponsors/7.png"></a>',
-        '<a href="http://www.aqua-check.co.uk/"><img src="images/sponsors/8.jpg"></a>',
-        '<a href="http://www.manchestereveningnews.co.uk/"><img src="images/sponsors/9.png"></a>',
-        '<a href="http://www.thebarbershopdidsbury.co.uk/"><img src="images/sponsors/10.png"></a>',
-        '<a href="http://www.markbratttravel.com/"><img src="images/sponsors/11.png"></a>'
+        {link:'http://smcreditunion.co.uk/', img:'1.png', rank: 0.5 - Math.random()},
+        {link:'http://www.bottonline.co.uk/', img:'2.png', rank: 0.5 - Math.random()},
+        {link:'http://www.wearecreation.co.uk/', img:'3.png', rank: 0.5 - Math.random()},
+        {link:'http://www.claytonhulme.co.uk/', img:'4.png', rank: 0.5 - Math.random()},
+        {link:'https://twitter.com/Wheildons/', img:'5.png', rank: 0.5 - Math.random()},
+        {link:'https://en-gb.facebook.com/M-J-Leeming-Opticians-158925407454109/', img:'6.png', rank: 0.5 - Math.random()},
+        {link:'http://www.barneswalker.co.uk', img:'7.png', rank: 0.5 - Math.random()},
+        {link:'http://www.aqua-check.co.uk/', img:'8.jpg', rank: 0.5 - Math.random()},
+        {link:'http://www.manchestereveningnews.co.uk/', img:'9.png', rank: 0.5 - Math.random()},
+        {link:'http://www.thebarbershopdidsbury.co.uk/', img:'10.png', rank: 0.5 - Math.random()},
+        {link:'http://www.markbratttravel.com/', img:'11.png', rank: 0.5 - Math.random()}
       ];
-
-      $scope.random = function() {
-        return 0.5 - Math.random();
-      };
       
       //example for js unit tests:
       this.awesomeThings = [ 
