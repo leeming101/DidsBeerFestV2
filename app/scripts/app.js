0f5f9f2c2a9abacb12fba4f6f3a5025bfec2bfc9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 21:02:41 +0000

    Deploy leeming101/DidsBeerFestV2 to github.com/leeming101/DidsBeerFestV2.git:builds

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,95 +9,95 @@
  * Main module of the application.
  */
 angular
-  .module('didsBeerFestV2App', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ngtweet',
-    'uiGmapgoogle-maps'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/find', {
-        templateUrl: 'views/find.html',
-        controller: 'FindCtrl',
-        controllerAs: 'find'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/privacy', {
-        templateUrl: 'views/privacy.html'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl',
-        controllerAs: 'contact'
-      })
-      .when('/volunteer', {
-        templateUrl: 'views/volunteer.html',
-        controller: 'VolunteerCtrl',
-        controllerAs: 'volunteer'
-      })
-      .when('/tickets', {
-        templateUrl: 'views/tickets.html',
-        controller: 'TicketsCtrl',
-        controllerAs: 'tickets'
-      })
-      .when('/goodcauses', {
-        templateUrl: 'views/goodcauses.html',
-        controller: 'GoodCausesCtrl',
-        controllerAs: 'goodcauses'
-      })
-      .when('/music', {
-        templateUrl: 'views/music.html',
-        controller: 'MusicCtrl',
-        controllerAs: 'music'
-      })
-      .when('/beer', {
-        templateUrl: 'views/beer.html',
-        controller: 'BeerCtrl',
-        controllerAs: 'beer'
-      })
-      .when('/cider', {
-        templateUrl: 'views/cider.html',
-        controller: 'CiderCtrl',
-        controllerAs: 'cider'
-      })
-      .when('/craft', {
-        templateUrl: 'views/craft.html',
-        controller: 'CraftCtrl',
-        controllerAs: 'craft'
-      })
-      .when('/raffle', {
-        templateUrl: 'views/raffle.html'
-      })
-      .when('/lastyear', {
-        redirectTo: '/lastyear'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-  .config(function(uiGmapGoogleMapApiProvider) {
-    uiGmapGoogleMapApiProvider.configure({
-        key: 'AIzaSyDUIkZxzGxRGNEgCWdjOylusJhFetr6qv4',
-        v: '3.23', //defaults to latest 3.X anyhow
-        libraries: 'weather,geometry,visualization'
-    });
-  });
+    .module('didsBeerFestV2App', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ngtweet',
+        'uiGmapgoogle-maps'
+    ])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/find', {
+                templateUrl: 'views/find.html',
+                controller: 'FindCtrl',
+                controllerAs: 'find'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .when('/privacy', {
+                templateUrl: 'views/privacy.html'
+            })
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'ContactCtrl',
+                controllerAs: 'contact'
+            })
+            .when('/volunteer', {
+                templateUrl: 'views/volunteer.html',
+                controller: 'VolunteerCtrl',
+                controllerAs: 'volunteer'
+            })
+            .when('/tickets', {
+                templateUrl: 'views/tickets.html',
+                controller: 'TicketsCtrl',
+                controllerAs: 'tickets'
+            })
+            .when('/goodcauses', {
+                templateUrl: 'views/goodcauses.html',
+                controller: 'GoodCausesCtrl',
+                controllerAs: 'goodcauses'
+            })
+            .when('/music', {
+                templateUrl: 'views/music.html',
+                controller: 'MusicCtrl',
+                controllerAs: 'music'
+            })
+            .when('/beer', {
+                templateUrl: 'views/beer.html',
+                controller: 'BeerCtrl',
+                controllerAs: 'beer'
+            })
+            .when('/cider', {
+                templateUrl: 'views/cider.html',
+                controller: 'CiderCtrl',
+                controllerAs: 'cider'
+            })
+            .when('/craft', {
+                templateUrl: 'views/craft.html',
+                controller: 'CraftCtrl',
+                controllerAs: 'craft'
+            })
+            .when('/raffle', {
+                templateUrl: 'views/raffle.html'
+            })
+            .when('/lastyear', {
+                redirectTo: '/lastyear'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }])
+    .config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyDUIkZxzGxRGNEgCWdjOylusJhFetr6qv4',
+            v: '3.23', //defaults to latest 3.X anyhow
+            libraries: 'weather,geometry,visualization'
+        });
+    }]);
 
-  window.addEventListener('load', function(e) {
+window.addEventListener('load', function(e) {
     if (window.applicationCache) {
         window.applicationCache.addEventListener('updateready', function(e) {
             if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
--- app/scripts/controllers/music.js
+++ app/scripts/controllers/music.js
@@ -8,23 +8,23 @@
  * Controller of the didsBeerFestV2App
  */
 angular.module('didsBeerFestV2App')
-  .controller('MusicCtrl', function () {   
-      $('#lnkAbout').removeClass('active');
-      $('#lnkContact').removeClass('active');
-      $('#lnkHome').removeClass('active');
-      $('#lnkFind').removeClass('active');
-      $('#lnkVolunteer').removeClass('active');
-      $('#lnkTickets').removeClass('active');
-      $('#lnkGoodCauses').removeClass('active');
-      $('#lnkMusic').addClass('active');
-      $('#lnkCider').removeClass('active');
-      $('#lnkBeer').removeClass('active');
-      $('#lnkCraft').removeClass('active');
+    .controller('MusicCtrl', [function() {
+        $('#lnkAbout').removeClass('active');
+        $('#lnkContact').removeClass('active');
+        $('#lnkHome').removeClass('active');
+        $('#lnkFind').removeClass('active');
+        $('#lnkVolunteer').removeClass('active');
+        $('#lnkTickets').removeClass('active');
+        $('#lnkGoodCauses').removeClass('active');
+        $('#lnkMusic').addClass('active');
+        $('#lnkCider').removeClass('active');
+        $('#lnkBeer').removeClass('active');
+        $('#lnkCraft').removeClass('active');
 
-      //example for js unit tests:
-      this.awesomeThings = [ 
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+        //example for js unit tests:
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    }]);
