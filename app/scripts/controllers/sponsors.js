18aad18432ff0a6a3f680626b245acc815185679
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-30 20:50:40 +0000

    Deploy leeming101/DidsBeerFestV2 to github.com/leeming101/DidsBeerFestV2.git:builds

--- app/scripts/controllers/sponsors.js
+++ app/scripts/controllers/sponsors.js
@@ -12,24 +12,25 @@ angular.module('didsBeerFestV2App')
 
         $scope.sponsorList = [
             { link: 'http://www.bottonline.co.uk/', img: 'images/sponsors/2.png', rank: 0.5 - Math.random() },
-            // { link: 'http://www.wearecreation.co.uk/', img: 'images/sponsors/3.png', rank: 0.5 - Math.random() },
-            // { link: 'http://smcreditunion.co.uk/', img: 'images/sponsors/1.png', rank: 0.5 - Math.random() },
-            // { link: 'https://twitter.com/Wheildons/', img: 'images/sponsors/5.png', rank: 0.5 - Math.random() },
+            { link: 'http://www.wearecreation.co.uk/', img: 'images/sponsors/3.png', rank: 0.5 - Math.random() },
+            { link: 'http://smcreditunion.co.uk/', img: 'images/sponsors/1.png', rank: 0.5 - Math.random() },
+            { link: 'https://twitter.com/Wheildons/', img: 'images/sponsors/5.png', rank: 0.5 - Math.random() },
             { link: 'http://www.claytonhulme.co.uk/', img: 'images/sponsors/4.png', rank: 0.5 - Math.random() },
             // { link: 'https://twitter.com/TheFletcherMoss', img: 'images/sponsors/17.png', rank: 0.5 - Math.random() },
             { link: 'https://en-gb.facebook.com/M-J-Leeming-Opticians-158925407454109/', img: 'images/sponsors/6.png', rank: 0.5 - Math.random() },
             { link: 'http://www.barneswalker.co.uk', img: 'images/sponsors/7.png', rank: 0.5 - Math.random() },
-            { link: 'http://www.thebarbershopdidsbury.co.uk/', img: 'images/sponsors/10.png', rank: 0.5 - Math.random() },
-            { link: 'https://elmhousetreeservices.co.uk/', img: 'images/sponsors/36.png', rank: 0.5 - Math.random() },
-            { link: 'https://southmanchesternews.co.uk/', img: 'images/sponsors/35.png', rank: 0.5 - Math.random() },
-            { link: 'http://www.eclectichotels.co.uk/didsbury-house/', img: 'images/sponsors/14.png', rank: 0.5 - Math.random() },
-            { link: 'https://www.yell.com/biz/burns-partnership-llp-manchester-1120012/', img: 'images/sponsors/37.svg', rank: 0.5 - Math.random() },
+            // { link: 'http://www.thebarbershopdidsbury.co.uk/', img: 'images/sponsors/10.png', rank: 0.5 - Math.random() },
+            // { link: 'https://elmhousetreeservices.co.uk/', img: 'images/sponsors/36.png', rank: 0.5 - Math.random() },
+            // { link: 'https://southmanchesternews.co.uk/', img: 'images/sponsors/35.png', rank: 0.5 - Math.random() },
+            // { link: 'http://www.eclectichotels.co.uk/didsbury-house/', img: 'images/sponsors/14.png', rank: 0.5 - Math.random() },
+            // { link: 'https://www.yell.com/biz/burns-partnership-llp-manchester-1120012/', img: 'images/sponsors/37.svg', rank: 0.5 - Math.random() },
             { link: 'http://www.geipel.co.uk/', img: 'images/sponsors/13.png', rank: 0.5 - Math.random() },
             { link: 'http://www.bollingtonbrewing.co.uk/', img: 'images/sponsors/19.png', rank: 0.5 - Math.random() },
             { link: 'http://www.chantrybrewery.co.uk/', img: 'images/sponsors/20.png', rank: 0.5 - Math.random() },
             { link: 'http://www.hireequip.co.uk/', img: 'images/sponsors/21.jpg', rank: 0.5 - Math.random() },
             { link: 'https://twitter.com/DidsburyGinFest', img: 'images/sponsors/38.png', rank: 0.5 - Math.random() },
-            { link: 'http://wdc-creative.com/', img: 'images/sponsors/24.png', rank: 0.5 - Math.random() }
+            { link: 'http://www.didsburytraders.co.uk/', img: 'images/sponsors/39.png', rank: 0.5 - Math.random() },
+            // { link: 'http://wdc-creative.com/', img: 'images/sponsors/24.png', rank: 0.5 - Math.random() }
 
             /*
             {link:'http://www.aqua-check.co.uk/', img:'images/sponsors/8.jpg', rank: 0.5 - Math.random()},
