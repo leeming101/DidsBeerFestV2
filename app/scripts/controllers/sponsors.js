b44ff91f7c17ee8db9b3930ecd219e789114f6f0
Paul Leeming <pleeming@gmail.com>
2019-10-08 23:04:09 +0100

    add the chippy sponsor

--- app/scripts/controllers/sponsors.js
+++ app/scripts/controllers/sponsors.js
@@ -32,6 +32,7 @@ angular.module('didsBeerFestV2App')
             { link: 'https://twitter.com/DidsburyGinFest', img: 'images/sponsors/38.png', rank: 0.5 - Math.random() },
             { link: 'http://www.didsburytraders.co.uk/', img: 'images/sponsors/39.png', rank: 0.5 - Math.random() },
             //{ link: 'https://www.beer52.com/', img: 'images/sponsors/42.png', rank: 0.5 - Math.random() },
+            { link: 'https://www.facebook.com/thechippyonburtonroad/', img: 'images/sponsors/43.png', rank: 0.5 - Math.random() },
             // { link: 'http://wdc-creative.com/', img: 'images/sponsors/24.png', rank: 0.5 - Math.random() }
 
             
